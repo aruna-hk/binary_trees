618e516278ee0303ed41eef9220b1ab57b19ea1a
kiptoo haron <kiptooharon.hk@gmail.com>
2024-02-28 13:52:48 +0300

    function to find tree balancing factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,22 @@
 #include "binary_trees.h"
-#include <stdio.h>
+/**
+* height - find heigh of binarytree
+* @tree: tree to find height
+* Return: height of the tree
+*/
+size_t height(const binary_tree_t *tree)
+{
+	size_t lefth = 0, righth = 0;
+
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	lefth = 1 + height(tree->left);
+	righth = 1 + height(tree->right);
+	if (lefth > righth)
+		return (lefth);
+	return (righth);
+}
 /**
 * binary_tree_balance - return tree balancing factor
 * @tree: tre to balance
@@ -12,8 +29,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		left = binary_tree_height(tree->left);
-		right = binary_tree_height(tree->right);
+		left = height(tree->left);
+		right = height(tree->right);
 
 		if (left == right)
 		{
