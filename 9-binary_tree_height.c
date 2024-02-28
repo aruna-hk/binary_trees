ca2c44cab2a564ba59cd437f8b5eb89ec211c73f
kiptoo haron <kiptooharon.hk@gmail.com>
2024-02-28 09:41:49 +0300

    finding height of tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+* binary_tree_height - find height of binary tree
+* @tree: pointer to root node of the tre
+* Return: height of the binary tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		size_t lefth = 0, righth = 0;
+
+		lefth = 1 + binary_tree_height(tree->left);
+		righth = 1 + binary_tree_height(tree->right);
+		if (lefth > righth)
+			return (lefth);
+		return (righth);
+	}
+	return (-1);
+}
