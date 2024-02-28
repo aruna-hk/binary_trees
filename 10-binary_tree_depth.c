c091dbbde3d84fa455408c5a5be88d513b535f67
kiptoo haron <kiptooharon.hk@gmail.com>
2024-02-28 11:46:37 +0300

    finding depth of node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,7 +6,5 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (-1);
-	return (1 + binary_tree_depth(tree->parent));
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent): 0);
 }
